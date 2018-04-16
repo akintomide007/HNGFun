4523f63f229c7f82b734c9f8bb7fae5a09403ee0
akintomide007 <adebileakintomide@gmail.com>
2018-04-16 22:41:53 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -2,7 +2,17 @@
 
 define ('DB_USER', "root");
 define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "hng_fun");
+<<<<<<< HEAD
+<<<<<<< HEAD
+define ('DB_DATABASE', "hng");
 define ('DB_HOST', "localhost");
+=======
+define ('DB_DATABASE', "hng-fun");
+define ('DB_HOST', "");
+>>>>>>> 02ede7b72e5bf5ec90c82e34a23df7a09c2f3153
 
+=======
+define ('DB_DATABASE', "");
+define ('DB_HOST', "");
+>>>>>>> 66eda08ee0c59ac9451cd518d7177f80a95dd4f0
 ?>
